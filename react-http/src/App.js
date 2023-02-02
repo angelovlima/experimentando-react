5d7ed465dc009bddeaf872d5010767da1c966d0a
angelo_lima <angelovictor2001@hotmail.com>
2023-02-02 10:50:51 -0300

    Definindo Rotas

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, NavLink, Route, Routes } from 'react-router-dom';
 
 import Usuarios from './components/Usuarios/Usuarios'
+import AdicionarUsuario from './components/AdicionarUsuario/AdicionarUsuario';
+import Home from './components/Home/Home';
+import PaginaNaoEncontrada from './components/PaginaNaoEncontrada/PaginaNaoEncontrada';
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <Usuarios />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/usuarios">Lista de Usuários</NavLink></li>
+              <li><NavLink to="/cadastro">Cadastro de Usuários</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/usuarios" element={<Usuarios />}/>
+            <Route path="/cadastro" element={<AdicionarUsuario />}/>
+            <Route path="*" element={<PaginaNaoEncontrada />}/>
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
